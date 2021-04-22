14998719e08fc3f39cfcaf4bf16b6ea8347f7e86
zackgithuboriginal <z.gibbons@hotmail.com>
2021-04-22 14:28:59 +0000

    Added home page contnet

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     """ A view to retyurn the index page""" 
-    
+
     return render(request, 'home/index.html')
