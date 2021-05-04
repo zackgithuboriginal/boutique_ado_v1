f95cebf6e33ca37065e0614ea67b9635ad51b67a
zackgithuboriginal <z.gibbons@hotmail.com>
2021-05-04 16:30:38 +0000

    added checkout views and template!

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,17 +2,17 @@ from django.contrib import admin
 from .models import Order, OrderLineItem
 
 
-class OrderLineItemAdminInLine(admin.TabularInline):
+class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
-    readonly_fields = ('lineitem_total')
+    readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
-    inlines = (OrderLineItemAdminInLine,)
+    inlines = (OrderLineItemAdminInline,)
 
-    readonly_fields = ('order_number', 'date'
-                        'delivery_cost', 'order_total',
-                        'grand_total',)
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
 
     fields = ('order_number', 'date', 'full_name', 
               'email', 'phone_number', 'country', 
--- checkout/signals.py
+++ checkout/signals.py
@@ -4,7 +4,7 @@ from .models import OrderLineItem
 
 
 @receiver(post_save, sender=OrderLineItem)
-def update_on_save(sender, instance, created, **kwargs)
+def update_on_save(sender, instance, created, **kwargs):
     """
     Update order total on lineitem updated/create
     """
@@ -12,7 +12,7 @@ def update_on_save(sender, instance, created, **kwargs)
 
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs)
+def delete_on_save(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
