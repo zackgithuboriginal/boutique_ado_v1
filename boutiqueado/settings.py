22ff53488c7ea0028d80b1b5e0e053371d663a33
zackgithuboriginal <z.gibbons@hotmail.com>
2021-05-12 14:18:19 +0000

    Fixing issues

--- boutiqueado/settings.py
+++ boutiqueado/settings.py
@@ -121,8 +121,6 @@ WSGI_APPLICATION = 'boutiqueado.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-
-
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
